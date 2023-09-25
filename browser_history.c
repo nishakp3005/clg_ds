2fff7089c75ba195ca903aea8e75e2cdb74fc8a6
Nishita <nishakp3005@gmail.com>
2023-09-25 23:40:26 +0530

    Doubly linked list added

--- browser_history.c
+++ browser_history.c
@@ -16,32 +16,56 @@ typedef struct BrowserHistory{
     BrowserNode* current;  
 } BrowserHistory;
 
+BrowserNode* createNode(char* url) {
+    BrowserNode* newNode = (BrowserNode*)malloc(sizeof(BrowserNode));
+    newNode->url = strdup(url);
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    return newNode;
+}
 
 // this creates a browser history
 BrowserHistory* browserHistoryCreate(char * homepage) {
-    
+    BrowserHistory* history = (BrowserHistory*)malloc(sizeof(BrowserHistory));
+    history->current = createNode(homepage);
+    return history;
 }
 
 
 // this visits a URL
 void browserHistoryVisit(BrowserHistory* obj, char * url) {
-  
+    while(obj->current->next != NULL){
+        BrowserNode* temp = obj->current->next;
+        obj->current->next = NULL;
+        free(temp->url);
+        free(temp);
+    }
+    BrowserNode* newNode = createNode(url);
+    newNode->prev = obj->current;
+    obj->current->next = newNode;
+    obj->current = newNode;
+
 }
 
 
 // this moves back a number of 'steps' in history 
-char * browserHistoryBack(BrowserHistory* obj, int steps) {
+// char * browserHistoryBack(BrowserHistory* obj, int steps) {
   
-}
+// }
 
 // this moves forward a number of 'steps' in history 
-char * browserHistoryForward(BrowserHistory* obj, int steps) {
+// char * browserHistoryForward(BrowserHistory* obj, int steps) {
   
-}
+// }
 
 // this cleans up the browser history and releases memory
-void browserHistoryFree(BrowserHistory* obj) {
+// void browserHistoryFree(BrowserHistory* obj) {
     
+// }
+
+int main(){
+    char* homepage = "google.com";
+    BrowserHistory* obj = browserHistoryCreate(homepage);
 }
 
 /**
